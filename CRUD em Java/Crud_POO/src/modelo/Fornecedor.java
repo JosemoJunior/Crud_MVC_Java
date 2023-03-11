7f132dd07f18d66276cdb7c2c3fbd84f2b8b2abe
JosemoJunior <103155008+JosemoJunior@users.noreply.github.com>
2023-03-11 11:57:11 -0300

    Adicionando a Classe Fornecedor

---
+++ CRUD em Java/Crud_POO/src/modelo/Fornecedor.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author josemojr
+ */
+public class Fornecedor {
+    private String nome;
+    private String cnpj;
+    private int idFornecedor;
+    
+    public Fornecedor () {
+        
+    }
+
+    public Fornecedor(String nome, String cnpj, int idFornecedor) {
+        this.nome = nome;
+        this.cnpj = cnpj;
+        this.idFornecedor = idFornecedor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public int getIdFornecedor() {
+        return idFornecedor;
+    }
+
+    @Override
+    public String toString() {
+        return "Fornecedor{" + "nome=" + nome + ", cnpj=" + cnpj + ", idFornecedor=" + idFornecedor + '}';
+    }
+    
+    
+    
+}
